898feb994f27f0dde00a74f2389d290c6b09797b
madamtalktrue <etoroobong11@gmail.com>
2022-10-27 11:51:47 -0700

    0x06_1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 /**
-<<<<<<< HEAD
   * _strncat - Concatenates two strings
   * @dest: The destination value
   * @src: The source value
@@ -9,15 +8,6 @@
   *
   * Return: A pointer to the resulting string dest
   */
-=======
- * _strncat - Concatenates two strings
- * @dest: The destination value
- * @src: The source value
- * @n: The limit of the concatenation
- * 
- * Return: A pointer to the resulting string dest
- */
->>>>>>> d175eca0ca7c252974f93ffb6c8a3d57958fb1b5
 char *_strncat(char *dest, char *src, int n)
 {
 	int dlen = 0, j = 0;
@@ -34,15 +24,7 @@ char *_strncat(char *dest, char *src, int n)
 		j++;
 	}
 
-<<<<<<< HEAD
 	dest[dlen + n + 1] = '\0';
 
 	return (dest);
 }
-=======
-		dest[dlen + n + 1] = '\0';
-
-		return (dest);
-}
-
->>>>>>> d175eca0ca7c252974f93ffb6c8a3d57958fb1b5
